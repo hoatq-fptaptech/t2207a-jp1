7635f10269b2574c50df15fbfc8f348a826552a6
macbook <quanghoa.trinh@gmail.com>
2023-01-11 14:18:19 +0700

    asm

---
+++ src/asm/PhoneNumber.java
@@ -0,0 +1,50 @@
+package asm;
+
+import java.util.ArrayList;
+
+public class PhoneNumber {
+    private String name;
+    private ArrayList<String> phones = new ArrayList<>();
+
+    public PhoneNumber() {
+    }
+
+    public PhoneNumber(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public ArrayList<String> getPhones() {
+        return phones;
+    }
+
+    public void setPhones(ArrayList<String> phones) {
+        this.phones = phones;
+    }
+
+    public void addPhone(String phone){
+        if(!phones.contains(phone)){
+            phones.add(phone);
+        }
+    }
+
+    public void updatePhone(String oldPhone,String newPhone){
+        if(phones.contains(oldPhone)){
+            int i = phones.indexOf(oldPhone);
+            phones.add(i,newPhone);
+        }
+    }
+
+    public void deletePhone(String phone){
+        if(phones.contains(phone)){
+            phones.remove(phone);
+        }
+    }
+}
