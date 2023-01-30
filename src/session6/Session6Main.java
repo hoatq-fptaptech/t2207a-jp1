6fab99b8ebf483f79d1651407aea2a255822b1a7
macbook <quanghoa.trinh@gmail.com>
2023-01-30 16:51:00 +0700

    session 6

---
+++ src/session6/Session6Main.java
@@ -0,0 +1,21 @@
+package session6;
+
+public class Session6Main {
+    public static void main(String[] args){
+//        Human h = new Human();
+        Asian a = new Asian();
+        American am = new American();
+        Human h = new Human() { // anonymous class
+            @Override
+            public void eat() {
+
+            }
+
+            @Override
+            public void sleep() {
+
+            }
+        };
+        
+    }
+}
