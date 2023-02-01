9f7f86922a6e357ebb57a9b5632454951e0806b6
macbook <quanghoa.trinh@gmail.com>
2023-02-01 14:30:41 +0700

    asm 11

---
+++ src/asm11/PhoneNumber.java
@@ -0,0 +1,30 @@
+package asm11;
+
+import java.util.ArrayList;
+
+public class PhoneNumber {
+    public String name;
+    public ArrayList<String> phones;
+
+    public PhoneNumber(String name, String phone) {
+        this.name = name;
+        this.phones = new ArrayList<>();
+        this.phones.add(phone);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public ArrayList<String> getPhones() {
+        return phones;
+    }
+
+    public void setPhones(ArrayList<String> phones) {
+        this.phones = phones;
+    }
+}
